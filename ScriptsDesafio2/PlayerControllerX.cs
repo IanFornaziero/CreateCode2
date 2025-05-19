875fedc3391b6fc0ffb9096fa05bf08a06abad60
IanFornaziero <capitaoian115@gmail.com>
2025-05-19 14:15:21 -0300

    Add files via upload

---
+++ ScriptsDesafio2/PlayerControllerX.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    private float floatForce = 50.0f;
+    private float gravityModifier = 1.5f;
+    private float yRange = 15.0f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>(); //resolve o problema de não aceitar o comando do espaço
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce); 
+        }else if (transform.position.y > yRange) //define um limite para o objeto não sair da tela
+        {
+            transform.position = new Vector3(transform.position.x,yRange,transform.position.z); //resolve o problema dele passar da visão da câmera
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (collision.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(collision.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (collision.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(collision.gameObject);
+
+        }else if (collision.gameObject.CompareTag("Ground")) //resolve o problema dele tocar no chão e ficar por lá
+        {
+            playerRb.AddForce(Vector3.up * 300); //dá um impulso no objeto para que ele não fique parado no chão
+        }
+
+    }
+
+}
